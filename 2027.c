031ce19dd9066f4235cc699d7368fbb65d23ed33
Luoxufeiyan <luoxufeiyan@gmail.com>
2014-08-08 16:40:01 +0800

    Create 2027.c

---
+++ 2027.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int num[5] = { 0 }, len, leni, t, ti;
+    char cache[100] = { 0 };
+    scanf("%d\n", &t);
+    for (ti = 0; ti < t; ti++)
+    {
+        gets(cache);
+        memset(num, 0, 5);
+        num[2] = 0;
+        num[3] = 0;
+        num[4] = 0;
+        len = strlen(cache);
+        for (leni = 0; leni < len; leni++)
+        {
+            if (cache[leni] == 'a') {num[0]++; }
+            if (cache[leni] == 'e') {num[1]++; }
+            if (cache[leni] == 'i') {num[2]++; }
+            if (cache[leni] == 'o') {num[3]++; }
+            if (cache[leni] == 'u') {num[4]++; }
+        }
+        printf("a:%d\n", num[0]);
+        printf("e:%d\n", num[1]);
+        printf("i:%d\n", num[2]);
+        printf("o:%d\n", num[3]);
+        printf("u:%d\n", num[4]);
+        if (ti != t - 1)
+        {
+            printf("\n");
+        }
+    }
+    return 0;
+}

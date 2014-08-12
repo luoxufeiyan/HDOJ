c0676fa20fbb376f34e61eb6c24c7dbcc550ef68
Luoxufeiyan <luoxufeiyan@gmail.com>
2014-08-12 15:22:29 +0800

    Update 2037.c

--- 2037.c
+++ 2037.c
@@ -1,42 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct c
+#include<stdio.h>
+int main()
 {
-    int x;
-    int y;
-    int ord;
-}d[100];
-
-int cmp(const struct c *a, const struct c *b)
-{
-    if ((*a).x == (*b).x)
-        return (*a).y - (*b).y;
-    else
-        return (*a).x - (*b).x;
+	int st[100], end[100];
+	int n, a, b,i,j;
+	int sum = 0, max = 0;
+	while (scanf("%d", &n) && n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			scanf("%d%d", &st[i], &end[i]);
+		}
+		for ( i = 0; i < n; i++)
+		{
+			for (j = i + 1; j<n; j++)
+			{
+				if (end[i]>end[j])
+				{
+					a = end[i];
+					end[i] = end[j];
+					end[j] = a;
+					b = st[i];
+					st[i] = st[j];
+					st[j] = b;
+				}
+			}
+		}
+		for (i = 0; i<n; i++)
+		if (st[i] >= max)
+		{
+			sum++;
+			max = end[i];
+		}
+		printf("%d\n", sum);
+	}
+	return 0;
 }
-int main(void)
-{
-    int i, j, n, max;
-    while (scanf("%d", &n), n)
-    {
-        for (max = i = 0; i < n; i++)
-        {
-            scanf("%d%d", &d[i].x, &d[i].y);
-            d[i].ord = 1;
-        }
-        qsort(d, n, sizeof(struct c), cmp);
-        d[n-1].ord = 1;
-        for (i = n - 2; i >= 0; i--)
-        {
-            for (j = i + 1; j < n; j++)
-            {
-                if (d[i].y <= d[j].x && d[i].ord < d[j].ord + 1)
-                    d[i].ord = d[j].ord + 1;
-            }
-            if (max < d[i].ord)
-                max = d[i].ord;
-        }
-        printf("%d\n", max);
-    }
-    return 0;
-}	

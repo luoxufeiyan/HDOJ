dac811bb1f372966f356071ee7857586ea657968
Luoxufeiyan <luoxufeiyan@gmail.com>
2014-10-19 18:48:48 +0800

    Update 2053.c

--- 2053.c
+++ 2053.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
-
 int main()
 {
-    int n,k,i;
-    while(scanf("%d",&n)!=EOF)
-    {
-        k=0;
-        for(i=1;i<=n;i++)
-        {
-            if(n%i==0)
-                k++;
-        }
-        if(k%2==0)
-            printf("0\n");
-        else
-            printf("1\n");
-    }
-    return 0;
+	int n, i, j;
+	while (scanf("%d", &n) != EOF)
+	{
+		j = 0;
+		for (i = 1; i <=n; i++)
+		{
+			if (n%i==0)
+			{
+				j++;
+			}
+		}
+		if (j % 2 == 0)
+		{
+			printf("0\n");
+		}
+		else
+		{
+			printf("1\n");
+		}
+	}
+	return 0;
 }

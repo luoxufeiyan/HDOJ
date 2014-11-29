de5977611a260593022d54bb4407709d77fc51e1
alls <luoxufeiyan@gmail.com>
2014-11-29 11:15:53 +0800

    Update 4931.cpp

--- 4931.cpp
+++ 4931.cpp
@@ -1,17 +1,20 @@
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
 using namespace std;
-int main () {
-    int cas;
-    scanf("%d", &cas);
-    while (cas--) {
-        int sum = 0, arr[10];
-        for (int i = 0; i < 6; i++) {
+int main()
+{
+    int gus;
+    scanf("%d", &gus);
+    while (gus--)
+    {
+        int sum = 0, arr[11];
+        for (int i = 0; i < 6; i++)
+        {
             scanf("%d", &arr[i]);
             sum += arr[i];
         }
-        sort (arr, arr + 6);
+        sort(arr, arr + 6);
         int ans = arr[5] + arr[4];
         sum -= (ans + arr[0]);
         printf("%s\n", ans > sum ? "Grandpa Shawn is the Winner!" : "What a sad story!");
